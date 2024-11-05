4b8909781bbe67fc2a49e47b7b7eddb019862382
Maeve Crossan <maevecrossan@gmail.com>
2024-11-05 09:41:22 +0000

    Add procfile and update requirements file for heroku deployment.

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wiqf1kczsn2)&+c$qr)pfyso+oz$!8#ktzr%(ajf+^m*(*76sr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-maevecrossan-djangoblog-p16frbzg1a3.ws.codeinstitute-ide.net',]
+ALLOWED_HOSTS = ['8000-maevecrossan-djangoblog-p16frbzg1a3.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
