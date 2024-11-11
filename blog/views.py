8bd7aeb5fbc73a9eee167508f4e03a83e484f60a
Maeve Crossan <maevecrossan@gmail.com>
2024-11-11 18:04:41 +0000

    Add previous button to pagination.

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,4 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1) #applies filter to database  
     template_name = "blog/index.html" #use this to change template name
-    paginate_by = 6
+    paginate_by = 6 # displays six posts at a time
