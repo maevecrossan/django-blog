ca1391e3637778314c389e498a01a9a3bfce41a1
Maeve Crossan <maevecrossan@gmail.com>
2024-11-13 12:32:52 +0000

    Added comment CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'), # sets a string (ft the slug) as the URL extension  # The slug path converter before the colon defines the data type as a slug, and the slug after the colon is the post.slug value passed from the template. You see this value in the URL path in the browser bar.
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+        views.comment_delete, name='comment_delete'),
 ]
